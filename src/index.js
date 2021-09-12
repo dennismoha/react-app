0f7f9099be60b5d8001b6edadfbcaaedded1e71c
Dennis Moha <dennismoha2@gmail.com>
2021-09-12 10:36:08 +0300

    test with theme

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import "font-awesome/css/font-awesome.min.css"; 
+
 
 
 ReactDOM.render(
@@ -10,3 +11,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
